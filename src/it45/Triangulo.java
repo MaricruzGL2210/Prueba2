618a16e4565d6ff5e71a745077be4430dd394a45
Maricruz GL <Maricruz GL@LAPTOP-E7CPQBNB>
2019-02-17 15:23:49 -0600

    cambio

--- src/it45/Triangulo.java
+++ src/it45/Triangulo.java
@@ -8,7 +8,7 @@ package it45;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.Graphics2D; 
 import java.awt.event.MouseEvent;
 
 /**
