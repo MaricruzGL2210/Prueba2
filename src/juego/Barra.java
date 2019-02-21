f8b25514bf141425d53650bb1fb74a44afd7da5a
mgonzalezle <mgonzalezle@vectoritcgroup.com>
2019-02-21 15:27:59 -0600

    Se agregan comentarios

--- src/juego/Barra.java
+++ src/juego/Barra.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package juego;
 
 import java.awt.Graphics2D;
@@ -10,25 +5,38 @@ import java.awt.Rectangle;
 
 /**
  *
- * @author vectormx
+ * La clase Barra
+ * Se crea un las porpiedades que se usaran para la barra 
+ * 
+ * @author Ahernandez / Mgonzalez
+ *
  */
 public class Barra extends NivelCast{
 
+    //Se declara constantes para propiedades de la barra
     private static final int ALTO = 10;
     private static final int ANCHO = 110;
+    
+    //Constantese para la posicion de las barras
     private static final int X_B1 = 0;
     private static final int X_B2 = 160;
     private static final int X_B3 = 330;
     private static final int Y_B = 25;
     
-    Object juego; 
+    Object juego;
+    
+    //metodo que recibe el objeto de juego y el nivel de jueho 
     public Barra(Object juego, int nivel){
+        //inicializador de juego y nivel
         inicializador(juego, nivel);
     }
     
+    //metodo que obtiene las propiedades de la barra
     public int obtenerAltura(){
        return Y_B + ALTO + 15;
    }
+    
+    //metodos que obtienen los limites de las barras que se integran en el juego
     public Rectangle obtenerLimiteBarra1(){
          return new Rectangle(X_B1, Y_B, ANCHO, ALTO);
     }
@@ -38,21 +46,27 @@ public class Barra extends NivelCast{
     public Rectangle obtenerLimiteBarra3(){
          return new Rectangle(X_B3, Y_B, ANCHO, ALTO);
     }
+    
+    //metodo que hace que se muestren las barras
     public void visualizarBarra(Graphics2D g2d){
         g2d.fillRect(X_B1, Y_B, ANCHO, ALTO);
         g2d.fillRect(X_B2, Y_B, ANCHO, ALTO);
         g2d.fillRect(X_B3, Y_B, ANCHO, ALTO);
     }
+    
+    //metodo abstracto que obtiene el nivel de juego y lo convierte en juego
     @Override
     void inicializador(Object juego, int nivel) {
+        //Validador de nivel
         if (nivel == 2) {
             this.juego = (Nivel2)juego;
         }
     }
 
+    //metodo abstracto que obtiene el nivel y lo valida.
     @Override
     void validaNivel(int nivel) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
